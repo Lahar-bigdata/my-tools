e96baf07efaaa8afad76978af4484eef308dc3a0
李亚垒（Robin） <liyalei@qiyi.com>
2021-08-06 21:05:02 +0800

    anno

---
+++ src/test/java/Test.java
@@ -0,0 +1,17 @@
+import lombok.Data;
+import self.robin.asm.Data2;
+
+/**
+ * @author Li Yalei - Robin
+ * @since: 2021/8/6 19:38
+ */
+
+@Data
+public class Test {
+
+    private String ss;
+
+    public static void main(String[] args) {
+
+    }
+}
